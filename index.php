6748df950cbf4da7b9650b10bc52c1f6741d811a
Dustin Locke <s3165079@students.fscj.edu>
2025-10-11 01:32:48 -0400

    Fix header and nav links: correct Home link, point unfinished links to under_construction.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         
     <h2>Administrators</h2>
     <ul>
-        <li><a href="product_manager">Manage Products</a></li>
+        <li><a href="under_construction.php">Manage Products</a></li>
         <li><a href="under_construction.php">Manage Technicians</a></li>
         <li><a href="under_construction.php">Manage Customers</a></li>
         <li><a href="under_construction.php">Create Incident</a></li>
