37d13656593b66a71c9614e91a827e0c3adec2da
Dustin Locke <s3165079@students.fscj.edu>
2025-12-01 00:16:43 -0500

    feat(customer): added dropdown for country selection

--- customer_manager/update_customer.php
+++ customer_manager/update_customer.php
@@ -48,8 +48,21 @@ $customer = get_customer_by_id($customer_id);
         <label>Postal Code:</label>
         <input type="text" name="postalCode" value="<?php echo htmlspecialchars($customer['postalCode']); ?>"><br>
 
-        <label>Country Code:</label>
-        <input type="text" name="countryCode" value="<?php echo htmlspecialchars($customer['countryCode']); ?>"><br>
+        <label>Country:</label>
+        <?php
+        $countries = get_all_countries();
+        $selected_country = $customer['countryCode']; 
+        ?>
+        <select name="countryCode">
+            <?php foreach ($countries as $country): ?>
+                <option value="<?php echo htmlspecialchars($country['countryCode']); ?>" 
+                    <?php echo ($country['countryCode'] === $selected_country) ? 'selected' : ''; ?>>
+                    <?php echo htmlspecialchars($country['countryName']); ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <br>
+
 
         <label>Phone:</label>
         <input type="text" name="phone" value="<?php echo htmlspecialchars($customer['phone']); ?>"><br>
@@ -63,7 +76,8 @@ $customer = get_customer_by_id($customer_id);
         <input type="submit" name="update_customer" value="Update Customer">
     </form>
 
-    <p><a href="index.php?action=search_customers">Search Customer</a></p>
+    <a href="index.php?action=search_customers">Search Customer</a> |
+    <a href="index.php">Home</a>
 </main>
 
 <?php include('../view/footer.php'); ?>
