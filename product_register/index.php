743d7d1f3c78a49572278681fe4b13418f78de48
Elijah Cesaire <elicesaire@gmail.com>
2025-12-06 13:46:07 -0500

    register product link fully functional
    Added new functions to customers_db.php file adding a get for customer email, confirming customer registration, and adding a new registration if non already exists.
    
    performed test with existing customer email "kelly@example.com" that is already in the database. everything looks fine so far with no issues.

--- product_register/index.php
+++ product_register/index.php
@@ -1,8 +1,7 @@
 <?php
 require('../model/database.php');
-require('../model/customer_db.php');
+require('../model/customers_db.php');
 require('../model/product_db.php');
-require('../model/registration_db.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
